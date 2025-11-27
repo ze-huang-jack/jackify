4a8979109c36aefbeb58b3039db276856310c065
zehuangjack <suqiuliang727@gmail.com>
2025-11-27 12:24:40 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -15,7 +15,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 
 const rootElement = document.getElementById('root');
